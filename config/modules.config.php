14192eb93b82cb24d2e45ba58393ac1622438185
Luiz G R Freitas <luiz.freitas@meisters.solutions>
2023-11-21 13:26:39 -0300

    Starting over

---
+++ config/modules.config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\Db',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\ZendFrameworkBridge',
+    'Application',
+];
