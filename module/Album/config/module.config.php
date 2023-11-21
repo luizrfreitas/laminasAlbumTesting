c910c71a6d56ba21ca2061cf718551b958bf87b4
Luiz G R Freitas <luiz.freitas@meisters.solutions>
2023-11-21 13:34:59 -0300

    Routing and controllers

--- module/Album/config/module.config.php
+++ module/Album/config/module.config.php
@@ -2,12 +2,31 @@
 
 namespace Album;
 
+use Laminas\Router\Http\Segment;
 use Laminas\ServiceManager\Factory\InvokableFactory;
 
 return [
     'controllers' => [
         'factories' => [
             Controller\AlbumController::class => InvokableFactory::class,
+        ],
+    ],
+    'router' => [
+        'routes' => [
+            'album' => [
+                'type'    => Segment::class,
+                'options' => [
+                    'route' => '/album[/:action[/:id]]',
+                    'constraints' => [
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ],
+                    'defaults' => [
+                        'controller' => Controller\AlbumController::class,
+                        'action'     => 'index',
+                    ],
+                ],
+            ],
         ],
     ],
     'view_manager' => [
---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Album\Controller;
+
+use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
